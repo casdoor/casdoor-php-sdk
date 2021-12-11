f034dd8ed2f5406ef7789abbe5419735848104f7
basakest <47746206+basakest@users.noreply.github.com>
2021-12-12 00:32:09 +0800

    feat: Add doPost() method (#6)

--- src/Util/Util.php
+++ src/Util/Util.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Casdoor\Util;
 
+use Casdoor\Auth\AuthConfig;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use Psr\Http\Message\StreamInterface;
+use stdClass;
+use GuzzleHttp\Exception\GuzzleException;
 
 /**
  * Class Util.
@@ -22,5 +25,25 @@ class Util
         $response = $client->send($request);
         $stream = $response->getBody();
         return $stream;
+    }
+
+    public static function doPost(string $action, array $queryMap, AuthConfig $authConfig): stdClass
+    {
+        $query = '';
+        foreach ($queryMap as $k => $v) {
+            $query .= sprintf('%s=%s&', $k, $v);
+        }
+        $url = sprintf('%s/api/%s?%sclientId=%s&clientSecret=%s', $authConfig->endpoint, $action, $query, $authConfig->clientId, $authConfig->clientSecret);
+
+        $client = new Client();
+        $request = new Request('POST', $url, ['content-type' => 'text/plain;charset=UTF-8']);
+        try {
+            $resp = $client->send($request);
+        } catch (GuzzleException $e) {
+            return null;
+        }
+        $respStream = $resp->getBody();
+        $response = json_decode($respStream->__toString());
+        return $response;
     }
 }
