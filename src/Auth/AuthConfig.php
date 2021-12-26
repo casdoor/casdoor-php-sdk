2b0fb4a2801f9b4ffd8acc9c86cfd9addc8fae3f
basakest <47746206+basakest@users.noreply.github.com>
2021-12-27 00:08:48 +0800

    feat: Use RS256 to sign JWT token (#14)

--- src/Auth/AuthConfig.php
+++ src/Auth/AuthConfig.php
@@ -14,17 +14,16 @@ class AuthConfig
     public string $endpoint;
     public string $clientId;
     public string $clientSecret;
-    public string $jwtSecret;
     public string $JwtPublicKey;
     public string $organizationName;
     public string $applicationName;
 
-    public function __construct(string $endpoint, string $clientId, string $clientSecret, string $jwtSecret, string $organizationName, string $applicationName)
+    public function __construct(string $endpoint, string $clientId, string $clientSecret, string $jwtPublicKey, string $organizationName, string $applicationName)
     {
         $this->endpoint         = $endpoint;
         $this->clientId         = $clientId;
         $this->clientSecret     = $clientSecret;
-        $this->jwtSecret        = $jwtSecret;
+        $this->jwtPublicKey     = $jwtPublicKey;
         $this->organizationName = $organizationName;
         $this->applicationName  = $applicationName;
     }
