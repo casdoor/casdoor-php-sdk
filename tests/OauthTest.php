f94f92374cfdcb8088fb8961870683a8eccd1b7c
Linvery <1253445120@qq.com>
2022-09-01 00:43:36 +0800

    Update OauthTest.php

--- tests/OauthTest.php
+++ tests/OauthTest.php
@@ -22,7 +22,7 @@ class OauthTest extends TestCase
         // $endpoint = 'http://127.0.0.1:8000';
         // $clientId = 'c64b12723aefb65a88ce';
         // $clientSecret = 'c0c9d483a87332751b2564635765d71c9f6a2e83';
-        // $jwtSecret = file_get_contents(dirname(__FILE__) . '/token_jwt_key.pem');
+        // $jwtSecret = file_get_contents(dirname(__FILE__) . '/public_key.pem');
         // $organizationName = 'built-in';
         // $applicationName = 'testApp';
         // User::initConfig($endpoint, $clientId, $clientSecret, $jwtSecret, $organizationName, $applicationName);
