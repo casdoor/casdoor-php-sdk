76acb1123ff7e4ca674538a1f20fdc2f6581cf88
Linvery <1253445120@qq.com>
2022-09-20 00:22:40 +0800

    fix: delete deafult value

--- src/Auth/Application.php
+++ src/Auth/Application.php
@@ -12,28 +12,28 @@ use Casdoor\Exceptions\CasdoorException;
  */
 class Application
 {
-    public string $owner = "admin";
+    public string $owner;
     public string $name;
     public string $createdTime;
-    public array $providers = [["name" => "provider_captcha_default", "canSignUp" => false, "canSignIn" => false, "canUnlink" => false, "prompted" => false, "alertType" => "None"]];
+    public array $providers;
     public string $displayName;
-    public string $logo = "https://cdn.casbin.org/img/casdoor-logo_1185x256.png";
+    public string $logo;
     public string $homepageUrl;
     public string $description;
-    public string $organization = "built-in";
-    public string $cert = "cert-built-in";
-    public bool   $enablePassword = true;
-    public bool   $enableSignUp = true;
-    public bool   $enableSigninSession = false;
-    public bool   $enableCodeSignin = false;
-    public bool   $enableSamlCompress = false;
+    public string $organization;
+    public string $cert;
+    public bool   $enablePassword;
+    public bool   $enableSignUp;
+    public bool   $enableSigninSession;
+    public bool   $enableCodeSignin;
+    public bool   $enableSamlCompress;
 
     public string $clientId;
     public string $clientSecret;
-    public array  $redirectUris = ["http://localhost:9000/callback"];
-    public string $tokenFormat = "JWT";
-    public int    $expireInHours = 168;
-    public int    $formOffset = 8;
+    public array  $redirectUris;
+    public string $tokenFormat;
+    public int    $expireInHours;
+    public int    $formOffset;
     public int    $refreshExpireInHours;
     public string $signupUrl;
     public string $signinUrl;
@@ -42,56 +42,7 @@ class Application
     public string $termsOfUse;
     public string $signupHtml;
     public string $signinHtml;
-    public array $signupItems = [
-        [
-            "name" => "ID",
-            "visible" => false,
-            "required" => true,
-            "rule" => "Random"
-        ],
-        [
-            "name" => "Username",
-            "visible" => true,
-            "required" => true,
-            "rule" => "None"
-        ],
-        [
-            "name" => "Display name",
-            "visible" => true,
-            "required" => true,
-            "rule" => "None"
-        ],
-        [
-            "name" => "Password",
-            "visible" => true,
-            "required" => true,
-            "rule" => "None"
-        ],
-        [
-            "name" => "Confirm password",
-            "visible" => true,
-            "required" => true,
-            "rule" => "None"
-        ],
-        [
-            "name" => "Email",
-            "visible" => true,
-            "required" => true,
-            "rule" => "Normal"
-        ],
-        [
-            "name" => "Phone",
-            "visible" => true,
-            "required" => true,
-            "rule" => "None"
-        ],
-        [
-            "name" => "Agreement",
-            "visible" => true,
-            "required" => true,
-            "rule" => "None"
-        ]
-    ];
+    public array $signupItems ;
     public static $authConfig;
 
     public static function initConfig(string $endpoint, string $clientId, string $clientSecret, string $jwtSecret, string $organizationName, string $applicationName): void
@@ -105,22 +56,22 @@ class Application
         $this->createdTime = date("y-m-d H:i:s", time());
     }
 
-    public function addApplication(Application $Application): bool
+    public function addApplication(): bool
     {
         if (!isset($this->name)) {
             throw new CasdoorException("name is empty");
         }
-        $postBytes = json_encode($Application, JSON_THROW_ON_ERROR);
+        $postBytes = json_encode($this, JSON_THROW_ON_ERROR);
         $resp = Util::doPost('add-application', [], $this->authConfig, $postBytes, false);
         return $resp->data == 'Affected';
     }
 
-    public function deleteApplication(Application $Application): bool
+    public function deleteApplication(): bool
     {
         if (!isset($this->name)) {
             throw new CasdoorException("name is empty");
         }
-        $postBytes = json_encode($Application, JSON_THROW_ON_ERROR);
+        $postBytes = json_encode($this, JSON_THROW_ON_ERROR);
 
         $resp = Util::doPost('delete-application', [], $this->authConfig, $postBytes, false);
 
--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -9,26 +9,100 @@ class ApplicationTest
     public function initConfig()
     {
         $endpoint = 'http://127.0.0.1:8000';
-        $clientId = 'c64b12723aefb65a88ce';
-        $clientSecret = 'c0c9d483a87332751b2564635765d71c9f6a2e83';
+        $clientId = '6a4030b5f9fd3d00456f';
+        $clientSecret = '5b17bf8c0505d94d5de7615f030bf3f8a9c0d8a0';
         $jwtSecret = file_get_contents(dirname(__FILE__) . '/public_key.pem');
         $organizationName = 'built-in';
         $applicationName = 'testApp';
         Application::initConfig($endpoint, $clientId, $clientSecret, $jwtSecret, $organizationName, $applicationName);
     }
 
+    /**
+     * Default applicationConfig parameters
+     */
+    public function applicationConfig(){
+        $app = new Application(User::$authConfig);
+        $app->name = "default";
+        $app->displayname=$app->name;
+        $app->owner = "admin";
+        $app->providers=[["name" => "provider_captcha_default", "canSignUp" => false, "canSignIn" => false, "canUnlink" => false, "prompted" => false, "alertType" => "None"]];
+        $app->logo = "https://cdn.casbin.org/img/casdoor-logo_1185x256.png";
+        $app->organization = "built-in";
+        $app->cert = "cert-built-in";
+        $app->redirectUris = ["http://localhost:9000/callback"];
+        $app->tokenFormat = "JWT";
+        $app->expireInHours = 168;
+        $app->formOffset = 8;
+        $app->signupItems = [
+            [
+                "name" => "ID",
+                "visible" => false,
+                "required" => true,
+                "rule" => "Random"
+            ],
+            [
+                "name" => "Username",
+                "visible" => true,
+                "required" => true,
+                "rule" => "None"
+            ],
+            [
+                "name" => "Display name",
+                "visible" => true,
+                "required" => true,
+                "rule" => "None"
+            ],
+            [
+                "name" => "Password",
+                "visible" => true,
+                "required" => true,
+                "rule" => "None"
+            ],
+            [
+                "name" => "Confirm password",
+                "visible" => true,
+                "required" => true,
+                "rule" => "None"
+            ],
+            [
+                "name" => "Email",
+                "visible" => true,
+                "required" => true,
+                "rule" => "Normal"
+            ],
+            [
+                "name" => "Phone",
+                "visible" => true,
+                "required" => true,
+                "rule" => "None"
+            ],
+            [
+                "name" => "Agreement",
+                "visible" => true,
+                "required" => true,
+                "rule" => "None"
+            ]
+        ];
+        $app->enablePassword = true;
+        $app->enableSignUp = true;
+        $app->enableSigninSession = false;
+        $app->enableCodeSignin = false;
+        $app->enableSamlCompress = false;
+        return $app;
+    }
+
     public function addApplication(){
         $this->initConfig();
-        $app = new Application(User::$authConfig);
-        $app->name = "test01";
-        $app->displayname="test01";
+        $app = $this->applicationConfig();
+        $app->name="testApp";   # Modify config parameters
+        $app->displayname="testCompanyApp";
         $res = $app->addApplication();
     }
 
     public function deleteApplication(){
         $this->initConfig();
-        $app = new Application(User::$authConfig);
-        $app->name = "test01";
+        $app = $this->applicationConfig();
+        $app->name="testApp";   # Modify config parameters
         $res = $app->deleteApplication();
     }
 }
